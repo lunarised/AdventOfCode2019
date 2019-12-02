981667fc9a8ad78df8306750a646f745f61f08a5
lunarised <j@p7.co.nz>
2019-12-03 06:58:29 +1300

    File Reading Working

--- 01/FuelFinder.java
+++ 01/FuelFinder.java
@@ -1,11 +1,39 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class FuelFinder{
-    public static void main(String[] vala){
-        int[] tVals = {12,14,1969,100756};
-        for (int a:tVals){
-            System.out.println(FuelNeeded(a));
-        }
-    }
-    public static int FuelNeeded(int inp){
-        return ((inp/3)-2);
-    }
+		public static void main(String[] vala){
+						File file = new File("input.txt");
+
+				try {
+
+						Scanner sc = new Scanner(file);
+						while (sc.hasNextLine()) {
+								int i = sc.nextInt();
+							 	System.out.println(i);
+						}
+						sc.close();
+				} 
+				catch (FileNotFoundException e) {
+						e.printStackTrace();
+				}
+		}
+		public static int FuelNeeded(int inp){
+				return ((inp/3)-2);
+		}
 }
+
+/*
+
+
+
+public class insertionSort {
+
+public static void main(String[] args) {
+
+
+ }
+}
+
+*/

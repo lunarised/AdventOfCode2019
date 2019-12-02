b117b6f7e84b870c0c317257051f4fc9fc8755b4
lunarised <j@p7.co.nz>
2019-12-03 07:08:07 +1300

    Fixed Tabs

--- 01/FuelFinder.java
+++ 01/FuelFinder.java
@@ -3,37 +3,21 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class FuelFinder{
-		public static void main(String[] vala){
-						File file = new File("input.txt");
-
-				try {
-
-						Scanner sc = new Scanner(file);
-						while (sc.hasNextLine()) {
-								int i = sc.nextInt();
-							 	System.out.println(i);
-						}
-						sc.close();
-				} 
-				catch (FileNotFoundException e) {
-						e.printStackTrace();
+	public static void main(String[] vala){
+		File file = new File("input.txt");
+		try {
+			Scanner sc = new Scanner(file);
+			while (sc.hasNextLine()) {
+				int i = sc.nextInt();
+			 	System.out.println(i);
 				}
+			sc.close();
+		} 
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
 		}
-		public static int FuelNeeded(int inp){
-				return ((inp/3)-2);
-		}
+	}
+	public static int FuelNeeded(int inp){
+		return ((inp/3)-2);
+	}
 }
-
-/*
-
-
-
-public class insertionSort {
-
-public static void main(String[] args) {
-
-
- }
-}
-
-*/
